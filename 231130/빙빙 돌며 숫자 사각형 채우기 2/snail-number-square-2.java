ccb0cdedb44c3c08e3972c73257dc71aff7a36d2
chrhfoq123 <87653317+chrhfoq123@users.noreply.github.com>
2023-11-30 16:05:11 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 215ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231130/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int n;
+    public static int m;
+    public static int[][] board;
+
+    public static boolean inRange(int x, int y){
+        return (x >= 0 && x < n && y >= 0 && y < m);
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        int x = 0, y = 0;
+        int dir = 2;
+
+        board[x][y] = 1;
+        for(int i=2; i<=n*m; i++){
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if(!inRange(nx, ny) || board[nx][ny] != 0){
+                dir = (dir + 1) % 4;
+            }
+
+            x += dx[dir];
+            y += dy[dir];
+
+            board[x][y] = i;
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
